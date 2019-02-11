664b54f7bf090632ea6aaae935a6f71bc92263d8
Gabriel Yuri Iwazaki <49596753819@info.local>
2019-02-11 17:24:46 -0200

    criação do script de manipulação

---
+++ SCRIPTS/SP_Medical_Group_2TT_DDL_MANIPULACAO.sql
@@ -0,0 +1,11 @@
+USE SP_MEDICAL_GROUP_2TT_Gabriel_Iwazaki;
+
+select * from clinica;
+select * from consultas;
+select * from ESPECIALIDADES;
+select * from MEDICOS;
+select * from PRONTUARIO;
+select * from SITUACAO;
+select * from TIPO_USUARIO;
+select * from USUARIOS;
+
